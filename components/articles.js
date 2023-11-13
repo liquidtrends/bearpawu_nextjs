96b772c2f90a262baa1344898c3217ee5c40699b
liquidtrends <ryan@liquidtrends.com>
2023-11-13 14:17:42 -0700

    Change background and padding

--- components/articles.js
+++ components/articles.js
@@ -5,7 +5,7 @@ const Articles = ({ articles }) => {
   return (
     <div className="w-full -mt-20">
       {articles.map((article, index) => (
-        <div key={index} className={index % 3 === 0 ? 'bg-[#4E8188] hover:bg-[#105963] hover:scale-105 transition ease-in-out duration-300' : index % 3 === 1 ? 'bg-[#6D6E6E] hover:[#6178a3] hover:scale-105 transition ease-in-out duration-300  no-underline' : 'bg-teal-700'}>
+        <div key={index} className={index % 3 === 0 ? 'bg-[#4E8188] hover:bg-[#105963] hover:scale-105 transition ease-in-out duration-300 py-8' : index % 3 === 1 ? 'bg-[#6D6E6E] hover:[#6178a3] hover:scale-105 transition ease-in-out duration-300  no-underline py-8' : 'bg-teal-700'}>
           <Card article={article} />
         </div>
       ))}
